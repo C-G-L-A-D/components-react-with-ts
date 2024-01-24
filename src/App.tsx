7977c118e07b1f2fee6be7aa33e2bc5e9231f1a4
lad <ladqmpw@163.com>
2024-01-24 15:52:49 +0800

    完善button组件样式和属性

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button, { ButtonType, ButtonSize } from './components/Button/Button'
+import Alert, { AlertType } from './components/Alert/Alert'
 
 const App: React.FC = () => {
   return (
@@ -9,16 +10,6 @@ const App: React.FC = () => {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <h1>hello world</h1>
-        <Button>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          Hello
-        </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
-          Hello
-        </Button>
-        <Button btnType={ButtonType.Link} href='https://www.baidu.com'>
-          Hello baidu
-        </Button>
       </header>
     </div>
   )
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -22,24 +22,35 @@ interface BaseButtonProps {
   href?: string
 }
 
+// React.ButtonHTMLAttributes<HTMLElement> 原生 button 属性
+// & 表示 交叉类型
+type NativeButtonProps = React.ButtonHTMLAttributes<HTMLElement> & BaseButtonProps
+// a 链接属性
+type AnchorButtonProps = React.AnchorHTMLAttributes<HTMLElement> & BaseButtonProps
+
+// Partial 讲 属性设置为可选
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
 const Button: React.FC<BaseButtonProps> = props => {
-  const { disabled, size, btnType, children, href } = props
+  // restProps 用于接受剩余参数，包括组件原生属性
+  const { className, disabled, size, btnType, children, href, ...restProps } = props
 
   // 默认拥有 btn 类名，后面集合动态添加
+
   const classes = classnames('btn', {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     disabled: btnType === ButtonType.Link && disabled // a 链接标签没有这个属性，需要动态添加
   })
-  if (btnType === ButtonType.Link && href) {
+  if (btnType === ButtonType.Link) {
     return (
-      <a className={classes} href={href}>
+      <a className={classes} href={href || '#'} {...restProps}>
         {children}
       </a>
     )
   } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     )
